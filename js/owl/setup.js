3c4cf5c739f21a40689d9c3611077c4aac40f12b
Iphills <f.iphills@gmail.com>
2021-08-06 22:39:12 -0300

    commit inicial

---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+/*function abrir1(){
+    window.location.href = "https://www.youtube.com/watch?v=aWnglSRmilc";
+}
+
+function abrir2(){
+    window.location.href = "https://www.nationalgeographicbrasil.com/";
+}*/
+
